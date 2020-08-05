45dbbb2847c7efe853fb3b5fda5d3af636d758c2
miplayer1 <mhiezely@hotmail.fr>
2020-08-05 13:56:29 +0000

    Initial commit

---
+++ js/breeedr.js
@@ -0,0 +1,58 @@
+const breeedr = [
+
+      " une rigueur méthodique",
+
+    " une posture de chercheur",
+
+      " le respect des savoirs des apprenants",
+
+      " l'esprit critique",
+
+      " esthétique et éthique",
+
+      " l'incarnation des paroles par l'exemple",
+
+
+
+      " risque, acceptation du nouveau et rejet de toute forme de discrimination",
+
+      " la réflexion critique sur la pratique",
+
+      " que l'identité culturelle soit reconnue et assumée",
+       " la conscience de l'inachèvement ",
+
+       " la reconnaissance que nous sommes des êtres conditionnés ",
+
+       " le respect de l'autonomie de l'être qui apprend ",
+
+       " du bon sens ",
+
+       " humilité, tolérance et lutte pour la défense des droits des éducateurs ",
+
+      " l'appréhension de la réalité ",
+
+       " joie et espérance ",
+
+       " la conviction de ce que le changement est possible ",
+
+      " de la curiosité",
+      "assurance, compétence professionnelle et générosité",
+
+      "de s'engager, de « se mouiller »,de prendre parti",
+
+      "de comprendre que l'éducation est une forme d'intervention dans et sur le monde",
+
+      "liberté et autorité",
+
+      "de prendre consciemment des décisions",
+
+      "de savoir écouter",
+
+      "de reconnaître que l'éducation est idéologique",
+
+      "la disponibilité et une ouverture d'esprit pour dialoguer",
+
+      "de vouloir du bien aux apprenants",
+
+
+  ];
